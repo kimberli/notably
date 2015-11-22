a8fc5f837acc62a7665f7e44a5e53a762e80c75d
Akshay Ravikumar <akshayravikumar@dhcp-18-111-107-17.dyn.MIT.EDU>
2015-11-22 18:16:03 -0500

    cleaned splash html

--- client/assets/js/session-controller.js
+++ client/assets/js/session-controller.js
@@ -1,5 +1,6 @@
 angular.module('notablyApp').controller('sessionController', function ($scope, $routeParams) {
 
     $scope.sessionId = $routeParams.sessionId;
+    var simplemde = new SimpleMDE({ element: document.getElementById("MyID"), spellChecker: true, hideIcons: ["side-by-side"]});
 
 });
