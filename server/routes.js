90d49f16846fee28db4891dc99453112e8f54c39
kimberli <kimberlizhong@gmail.com>
2015-11-22 18:34:06 -0500

    dummy auth endpoint

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,14 @@
 // PACKAGES //
 router = require('express').Router();
 path = require('path');
+utils = require('./utils');
 
-// VIEW ENDPOINTS //
+// API ENDPOINTS //
+router.post('/api/auth', function(req, res) {
+    utils.sendSuccessResponse(res, { user : "kim" });
+});
+
+// VIEW ENDPOINT //
 router.get('/*', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../client/views/index.html'));
 });
