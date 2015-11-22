1d0b7e7e471f5dbe51994be0f0cb19d4195cdd62
kimberli <kimberlizhong@gmail.com>
2015-11-22 16:13:39 -0500

    fix routing

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@ router = require('express').Router();
 path = require('path');
 
 // VIEW ENDPOINTS //
-router.get('/', function(req, res, next) {
+router.get('/*', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../client/views/index.html'));
 });
 
