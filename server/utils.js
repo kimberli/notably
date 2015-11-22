d34ed2565f72b6b5d952724dddfff1c87d09f1c8
kimberli <kimberlizhong@gmail.com>
2015-11-22 17:47:01 -0500

    add utils for sending responses

---
+++ server/utils.js
@@ -0,0 +1,35 @@
+module.exports = (function () {
+
+    var _utils = {};
+
+    /**
+     * Helper function to send a success (200 OK) response
+     *
+     * @param res {object} - response object
+     * @param content {object} - content to send
+     */
+    _utils.sendSuccessResponse = function(res, content) {
+        res.status(200).json({
+            success: true,
+            content: content
+        }).end();
+    };
+
+    /**
+     * Helper function to send a failure response
+     *
+     * @param res {object} - response object
+     * @param errcode {number} - HTTP status code to send
+     * @param error {string} - error message to send
+     */
+    _utils.sendErrResponse = function(res, errcode, error) {
+        res.status(errcode).json({
+            success: false,
+            error: error
+        }).end();
+    };
+
+    Object.freeze(_utils);
+    return _utils;
+
+})();
