998f93582bbf16f16c4579651f17df828b016322
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-11-23 12:47:21 -0500

    populate course view

--- client/assets/js/course-controller.js
+++ client/assets/js/course-controller.js
@@ -2,4 +2,8 @@ angular.module('notablyApp').controller('courseController', function ($scope, $r
 
     $scope.courseNumber = $routeParams.courseNumber;
 
+    $scope.sessions = [{'title': 'stash111', 'count': 14},
+        {'title': 'hi22', 'count': 201},
+        {'title': 'heyo', 'count': 2}]
+
 });
