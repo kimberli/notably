379ddf4ecdcef699e3324a2e1516346d1411abc5
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-11-23 03:27:44 -0500

    profile view courses api call

--- client/assets/js/profile-controller.js
+++ client/assets/js/profile-controller.js
@@ -1,5 +1,14 @@
-angular.module('notablyApp').controller('profileController', function ($scope, $routeParams) {
+angular.module('notablyApp').controller('profileController', function ($scope, $routeParams, $http) {
 
     $scope.username = $routeParams.username;
+    $scope.courses = []
+
+    $http.get('/api/user/courses?username=' + $scope.username).then(function (response) {
+        if (response.status === 200) {
+            $scope.courses = response.data;
+        } else {
+            console.log($scope.data);
+        }
+    })
 
 });
