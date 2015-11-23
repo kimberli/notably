3376e4bec5c020eee0eacfb20f563a23a459e250
Alex Luh <aluh@mit.edu>
2015-11-22 20:04:30 -0500

    Fix format

--- server/models/Session.js
+++ server/models/Session.js
@@ -4,11 +4,11 @@ var Stash = require('./Stash');
 var Snippet = require('./Snippet');
 
 var sessionSchema = mongoose.Schema({
-  title: String,
-  timeCreated: Date,
-  course: {type: mongoose.Schema.Types.ObjectId, ref:'Class'},
-  stashes: [{type: mongoose.Schema.Types.ObjectId, ref:'Stash'}],
-  feed: [{type: mongoose.Schema.Types.ObjectId, ref:'Snippet'}]
+    title: String,
+    timeCreated: Date,
+    course: {type: mongoose.Schema.Types.ObjectId, ref:'Class'},
+    stashes: [{type: mongoose.Schema.Types.ObjectId, ref:'Stash'}],
+    feed: [{type: mongoose.Schema.Types.ObjectId, ref:'Snippet'}]
 });
 
 var Session = mongoose.model('Session', sessionSchema);
