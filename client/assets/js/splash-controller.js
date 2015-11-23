bba761a8440040b83a2e0dcbbd26a1aaf37a01f5
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-11-23 17:51:45 -0500

    splash form

--- client/assets/js/splash-controller.js
+++ client/assets/js/splash-controller.js
@@ -1,5 +1,17 @@
 angular.module('notablyApp').controller('splashController', function ($scope, $http) {
 
+    $scope.loginForm = false;
+    $scope.registerForm = false;
+
+    $scope.showLoginForm = function () {
+        $scope.loginForm = true;
+    }
+
+    $scope.showRegisterForm = function () {
+        $scope.loginForm = true;
+        $scope.registerForm = true;
+    }
+
     $scope.login = function () {
         $http.post('/api/user/login', {
             'username': 'vfazel',
