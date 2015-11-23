fc9740832b5c6e3b92d1b0c22d10c4989379645f
Akshay Ravikumar <akshayravikumar@dhcp-18-111-107-17.dyn.MIT.EDU>
2015-11-23 14:18:55 -0500

    lol

--- client/assets/js/class-controller.js
+++ client/assets/js/class-controller.js
@@ -1,5 +1,6 @@
 angular.module('notablyApp').controller('classController', function ($scope, $routeParams) {
 
     $scope.classNumber = $routeParams.classNumber;
+    $scope.sessions = [{title: "lol", date:"lol"},{title: "lol", date:"lol"},{title: "lol", date:"lol"},{title: "lol", date:"lol"}]
 
 });
