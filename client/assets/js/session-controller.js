bc44da26e09b59827826c3cc561c1fc63e221b76
Akshay Ravikumar <akshayravikumar@dhcp-18-111-107-17.dyn.MIT.EDU>
2015-11-23 17:17:03 -0500

    add methods

--- client/assets/js/session-controller.js
+++ client/assets/js/session-controller.js
@@ -9,62 +9,87 @@ angular.module('notablyApp').controller('sessionController', function ($scope, $
       saves: 3,
       content : "i direct hack",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "aluh",
       flags : 2,
       saves: 3,
       content : "Fresh Fruit in Lounge (EOM)",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "faz",
       flags : 2,
       saves: 3,
       content : "what do u mean by chill",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "kim",
       flags : 2,
       saves: 3,
       content : "i direct hack",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "aluh",
       flags : 2,
       saves: 3,
       content : "Fresh Fruit in Lounge (EOM)",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "faz",
       flags : 2,
       saves: 3,
       content : "what do u mean by chill",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "kim",
       flags : 2,
       saves: 3,
       content : "i direct hack",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "aluh",
       flags : 2,
       saves: 3,
       content : "Fresh Fruit in Lounge (EOM)",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     },{
       creator : "faz",
       flags : 2,
       saves: 3,
       content : "what do u mean by chill",
       savedBy : ["ayy", "lmao"],
-      flaggedBy : ["lmao","ayy"]
+      flaggedBy : ["lmao","ayy"],
+      id: "aCd21as"
     }];
+
+    $scope.addSnippet = function() {
+      alert($scope.snippetInput);
+      // TODO add http call
+    }
+
+    $scope.flagSnippet = function(id) {
+      alert("flag " + id);
+      // TODO add http call
+    }
+
+    $scope.removeSnippet = function(id) {
+      alert("remove " + id);
+      // TODO add http call
+    }
+
 });
