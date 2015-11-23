c3212cff1db9ed3bf5a4b39474d08ed14d61c43f
kimberli <kimberlizhong@gmail.com>
2015-11-23 03:54:44 -0500

    add /api/course/all route

--- server/routes/course.js
+++ server/routes/course.js
@@ -5,3 +5,18 @@ utils = require('../utils');
 Course = require('../models/Course');
 
 module.exports = router;
+
+/**
+ * GET - /api/course/all
+ */
+router.get('/all', function(req, res) {
+    if (req.currentUser) {
+        Course.getAllCourses(function(err,result) {
+            if (err) {
+                utils.sendErrResponse(res, 403, err);
+            } else {
+                utils.sendSuccessResponse(res, result);
+            }
+        });
+    } else utils.sendErrResponse(res, 403, 'Must be logged in');
+});
