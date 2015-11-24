fae8c154e8892b94f806c92ec278a462d424bba8
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-11-23 21:37:08 -0500

    course view

--- client/assets/js/course-controller.js
+++ client/assets/js/course-controller.js
@@ -1,40 +1,19 @@
-angular.module('notablyApp').controller('courseController', function ($scope, $routeParams) {
+angular.module('notablyApp').controller('courseController', function ($scope, $http, $routeParams) {
 
 
-    $http.get('/course?number=' + $routeParams.courseNumber).then(function (response) {
+    $http.get('/api/course?number=' + $routeParams.courseNumber).then(function (response) {
         $scope.course = response.data;
-        $scope.course.sessions = [{
-            "_id": "5653a2c3643c44825db45ed6",
-            "title": "6.170 Lecture 7",
-            "createdAt": "2015-11-24T02:18:00.394Z",
-            "activeUsers": 5
-        }, {
-            "_id": "5653a2c3643c44825db45ed6",
-            "title": "6.170 Lecture 6",
-            "createdAt": "2015-11-24T02:18:00.394Z",
-            "activeUsers": 10
-        }, {
-            "_id": "5653a2c3643c44825db45ed6",
-            "title": "6.170 Lecture 5",
-            "createdAt": "2015-11-24T02:18:00.394Z",
-            "activeUsers": 13
-        }, {
-            "_id": "5653a2c3643c44825db45ed6",
-            "title": "6.170 Lecture 4",
-            "createdAt": "2015-11-24T02:18:00.394Z",
-            "activeUsers": 7
-        }];
-    });
 
-    $scope.createSession = function () {
-      $http.post('/api/course/newsession', {
-          'number': $scope.courseNumber,
-          'title': $scope.sessionInput
-      }).then(function (response) {
-           Materialize.toast('Your session has been created!', 2000);
-      }, function(response) {
-          alert(response.data.error);
-      });
-    }
+        $scope.createSession = function () {
+            $http.post('/api/course/newsession', {
+                'number': $scope.course.meta.number,
+                'title': $scope.sessionInput
+            }).then(function (response) {
+                Materialize.toast('Your session has been created!', 2000);
+            }, function(response) {
+                alert(response.data.error);
+            });
+        }
+    });
 
 });
