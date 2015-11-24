3293e6be0347ab805f4e57132ffec52197d966a0
kimberli <kimberlizhong@gmail.com>
2015-11-23 21:10:46 -0500

    add user stash to /api/session GET

--- server/routes/session.js
+++ server/routes/session.js
@@ -9,11 +9,18 @@ Session = require('../models/Session');
  */
 router.get('/', function(req, res) {
     if (req.currentUser) {
-        Session.findSession(req.query.sessionId, function(err,result) {
+        Session.findSession(req.query.sessionId, function(err,session) {
             if (err) {
                 utils.sendErrResponse(res, 403, err);
             } else {
-                utils.sendSuccessResponse(res, result);
+                Stash.findBySessionAndUsername(result._id, req.currentUser, function(err, stash) {
+                    if (err) {
+                        utils.sendErrResponse(res, 403, err);
+                    } else {
+                        session.stash = stash;
+                        utils.sendSuccessResponse(res, session);
+                    }
+                });
             }
         });
     } else utils.sendErrResponse(res, 403, 'Must be logged in');
