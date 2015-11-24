e2b67d93429b673e6342c1debdc0b633f5a1c11d
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-11-23 21:30:57 -0500

    get course info

--- client/assets/js/course-controller.js
+++ client/assets/js/course-controller.js
@@ -1,9 +1,29 @@
 angular.module('notablyApp').controller('courseController', function ($scope, $routeParams) {
 
-    $scope.courseNumber = $routeParams.courseNumber;
 
-    $scope.sessions = [{'title': 'stash111', 'count': 14},
-        {'title': 'hi22', 'count': 201},
-        {'title': 'heyo', 'count': 2}]
+    $http.get('/course?number=' + $routeParams.courseNumber).then(function (response) {
+        $scope.course = response.data;
+        $scope.course.sessions = [{
+            "_id": "5653a2c3643c44825db45ed6",
+            "title": "6.170 Lecture 7",
+            "createdAt": "2015-11-24T02:18:00.394Z",
+            "activeUsers": 5
+        }, {
+            "_id": "5653a2c3643c44825db45ed6",
+            "title": "6.170 Lecture 6",
+            "createdAt": "2015-11-24T02:18:00.394Z",
+            "activeUsers": 10
+        }, {
+            "_id": "5653a2c3643c44825db45ed6",
+            "title": "6.170 Lecture 5",
+            "createdAt": "2015-11-24T02:18:00.394Z",
+            "activeUsers": 13
+        }, {
+            "_id": "5653a2c3643c44825db45ed6",
+            "title": "6.170 Lecture 4",
+            "createdAt": "2015-11-24T02:18:00.394Z",
+            "activeUsers": 7
+        }];
+    });
 
 });
