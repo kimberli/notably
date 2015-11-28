5526b4d2dc9b90cabe20706bbdea08b197f64641
Akshay Ravikumar <akshayravikumar@dhcp-18-111-107-17.dyn.MIT.EDU>
2015-11-28 18:13:00 -0500

    remove tooltip for now

--- client/assets/js/stash-controller.js
+++ client/assets/js/stash-controller.js
@@ -1,8 +1,8 @@
 angular.module('notablyApp').controller('stashController', function ($scope, $http, $rootScope, $location, $routeParams) {
     $scope.stashId = $routeParams.stashId;
+    $('.tooltipped').tooltip({delay: 50});
 
     $http.get('/api/stash?stashId=' + $scope.stashId).then(function (response) {
-          $('.tooltipped').tooltip({delay: 50});
           if (response.status === 200) {
               $scope.stash = response.data;
               $scope.snippets = $scope.stash.snippets;
