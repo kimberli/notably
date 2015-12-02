93b1c0808fcf7d208f4e7c11ff333d96904f5901
Alex Luh <aluh@mit.edu>
2015-12-02 17:22:38 -0500

    Frontend subscribe functionality

--- client/assets/js/course-controller.js
+++ client/assets/js/course-controller.js
@@ -4,7 +4,18 @@ angular.module('notablyApp').controller('courseController', function ($scope, $h
 
     $http.get('/api/course?number=' + $routeParams.courseNumber).then(function (response) {
         $scope.course = response.data;
-        $scope.loadPage();
+        $http.get('/api/user?username=' + $scope.currentUser).then(function (response) {
+            $scope.user = response.data;
+            $scope.subscribed = false;
+            if ($scope.user.courses.length !== 0) {
+                for (var i = 0; i < $scope.user.courses.length; i++) {
+                    if ($scope.user.courses[i].number === $scope.course.meta.number) {
+                        $scope.subscribed = true;
+                    }
+                }
+            }
+            $scope.loadPage();
+        });
     });
 
 
@@ -37,6 +48,7 @@ angular.module('notablyApp').controller('courseController', function ($scope, $h
                 'course': courseNumber
             }).then(function (response) {
                 Materialize.toast('You have subscribed to this course', 2000);
+                $scope.subscribed = true;
             }, function(response) {
                 Materialize.toast(response.data.error, 2000);
             });
@@ -48,6 +60,7 @@ angular.module('notablyApp').controller('courseController', function ($scope, $h
                 'course': courseNumber
             }).then(function (response) {
                 Materialize.toast('You have unsubscribed from this course', 2000);
+                $scope.subscribed = false;
             }, function(response) {
                 Materialize.toast(response.data.error, 2000);
             });
