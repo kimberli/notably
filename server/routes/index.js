1338d7363ccb85be5a6e0e5236d1ac293d665ad5
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-12-03 05:17:13 -0500

    make view routing more secure server-side

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,17 @@ router = require('express').Router();
 path = require('path');
 utils = require('../utils');
 
-// VIEW ENDPOINT //
-router.get('/*', function(req, res, next) {
+// VIEW ENDPOINTS //
+router.get('/', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../../client/views/index.html'));
+});
+
+router.get('/*', function(req, res, next) {
+    if (req.currentUser) {
+        res.sendFile(path.join(__dirname, '../../client/views/index.html'));
+    } else {
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
